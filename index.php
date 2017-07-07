22aa0fcbcca8573e4c5f28363472e144166a7e2c
Arcanae <justin.courdesse@gmail.com>
2017-07-07 12:00:21 +0200

    bootstrap

--- index.php
+++ index.php
@@ -17,19 +17,30 @@ foreach ($array as $file) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My Awesome Blog</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 <body>
-    <h1>My Awesome Blog</h1>
-    <nav><a href="create.html">New Post</a></nav>
-    <?php foreach ($files as $file) { ?>
-    <article>
-        <h2><?php echo basename($file, '.txt'); ?></h2>
-        <p><?php echo file_get_contents('post/' . $file); ?></p>
-        <form action="delete-post.php" method="POST">
-            <input type="hidden" name="filename" value="<?php echo $file; ?>">
-            <input type="submit" value="delete">
-        </form>
-    </article>
-    <?php } ?>
+    <style>
+    article {
+        background-color: lightblue;
+        margin-bottom: 1em;
+    }
+    </style>
+    <div class="container">
+        <h1 class="text-center col-sm-4 col-sm-offset-4">My Awesome Blog</h1>
+    </div>
+    <nav class="text-center col-sm-4 col-sm-offset-4"><a href="create.html">New Post</a></nav>
+    <div class="fluid text-justify">
+        <?php foreach ($files as $file) { ?>
+            <article class="col-sm-3 col-sm-offset-2 col-xs-8 col-xs-offset-2">
+                <h2 class="text-center"><?php echo basename($file, '.txt'); ?></h2>
+                <p class="text-center"><?php echo file_get_contents('post/' . $file); ?></p>
+                <form class="text-center" action="delete-post.php" method="POST">
+                    <input type="hidden" name="filename" value="<?php echo $file; ?>">
+                    <input type="submit" value="delete">
+                </form>
+            </article>
+        <?php } ?>
+    </div>
 </body>
 </html>
